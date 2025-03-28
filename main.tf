3b58a81d3fadd20a02d5954a89b498d8804b26d7
Chandra <smikcp@gmail.com>
2025-03-28 17:56:51 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucket"
+  location      = "US"
+  force_destroy = true
+project = "gcp-poc-313408"
+  public_access_prevention = "enforced"
+}
