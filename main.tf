31792883728b85cc7694518fc511dbbe20cde888
Chandra <smikcp@gmail.com>
2025-03-28 18:55:28 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-provider "google" {
-  project = "gcp-poc-313408"
-  region  = "us-central1"
-  #   credentials = "tf-cp.json"
-}
+# provider "google" {
+#   region  = "us-central1"
+#   #   credentials = "tf-cp.json"
+# }
 
 resource "google_storage_bucket" "cp-bucket" {
   name                     = "cp-no-public-access-bucket"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+    project = "gcp-poc-313408"
+
 }
 #
